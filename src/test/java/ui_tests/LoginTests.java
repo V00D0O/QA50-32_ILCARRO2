716ce83a823b776eadc99c3c084af8e07faabe5a
pass12345 <tipografia116@gmail.com>
2026-01-17 21:08:06 +0200

    start_progect

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,24 @@
+package ui_tests;
+
+import dto.User;
+import manager.ApplicationManager;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+    @Test
+    public void loginPositiveTest(){
+        User user = User.builder()
+                .email("sima_simonova370@gmail.com")
+                .password("BSas124!")
+                .build();
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        loginPage.clickBtnYalla();
+        Assert.assertTrue(loginPage.isLoggedInDisplayed());
+    }
+}
